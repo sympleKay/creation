a21e676eacf04e124358c1e6ec9d0c8a80480fde
sympleKay <kaylove4real10@gmail.com>
2020-06-15 18:18:05 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let app = express();
+
+let folderPath = process.argv[2];
+
+/* serves all the static files*/
+app.get(/^(.+)$/, function (req, res) {
+    console.log("static file request : " + req.params);
+    res.sendFile( (folderPath ? folderPath : __dirname) + req.params[0]);
+});
+
+
+let port = process.env.PORT || 2000;
+app.listen (port, function() {
+    console.log("Listening on port " + port);
+})
